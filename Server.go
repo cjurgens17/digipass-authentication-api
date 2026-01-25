d6cb1e4fd64fe80795e0c1a089c48148e3fd668e
Chris Jurgens <chrisjurgens12317@gmail.com>
2026-01-24 19:04:48 -0500

    Add DB connection. Make App struct to point to db pool. Init context and db pool in Main. Install postgres driver. Install godotenv. Attempt to load env for local, use railway if not available.

--- Server.go
+++ Server.go
@@ -1,12 +1,29 @@
 package main
 
 import (
+	"context"
+	"log"
 	"net/http"
+
 	"github.com/labstack/echo/v5"
 	"github.com/labstack/echo/v5/middleware"
+	"github.com/joho/godotenv"
 )
 
+
 func main() {
+	if err := godotenv.Load(); err != nil {
+		log.Println("No .env file found. If running Local add .env or else railway variables are being used")
+	}
+
+	ctx := context.Background()
+	pool := NewDBPool(ctx)
+
+	app := &App{
+		DB: pool,
+	}
+	defer app.DB.Close()
+
 	e := echo.New()
 	e.Use(middleware.RequestLogger())
 
