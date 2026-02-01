8f4824a1dcdd1c64700057b5b33c2300e751a394
Chris Jurgens <chrisjurgens12317@gmail.com>
2026-02-01 15:01:28 -0500

    Update /account/new to create tenant and add relation to account

--- services/Account.go
+++ services/Account.go
@@ -1,9 +1,9 @@
 package services
 
 import (
-	"gorm.io/gorm"
 	"DigiPassAuthenticationApi/packages/models"
 	"errors"
+	"gorm.io/gorm"
 )
 
 type AccountService struct {
@@ -16,19 +16,49 @@ func NewAccountService(db *gorm.DB) *AccountService {
 	}
 }
 
-func (s *AccountService) CreateAccount(name string, email string) (*models.Account, error){
-	if name == "" || email == ""{
+func (s *AccountService) CreateAccount(name string, email string) (*models.Account, error) {
+	if name == "" || email == "" {
 		return nil, errors.New("Name and email are required")
 	}
 
-	account := &models.Account {
-		Name: name,
-		Email: email,
+	//Open a transaction for rollbacks
+	tx := s.db.Begin()
+	defer func() {
+		if r := recover(); r != nil {
+			tx.Rollback()
+		}
+	}()
+
+	//1. Create Account
+	account := &models.Account{
+		Name:   name,
+		Email:  email,
 		Status: "active",
 	}
 
-	if err := s.db.Create(account).Error; err != nil {
-		return nil,err
+	if err := tx.Create(account).Error; err != nil {
+		return nil, err
 	}
+
+	//Tenant Service
+	ts := NewTenantService(s.db)
+
+	//2. Create Tenant
+	slug, err := ts.CreateUniqueTenantSlug(s.db)
+	if err != nil {
+		return nil, err
+	}
+
+	tenant := &models.Tenant {
+		AccountID: account.ID,
+		Slug: slug, 
+	}
+
+	if err := tx.Create(tenant).Error; err != nil {
+		return nil, err
+	}
+
+	//3. Link Creating User to AccountUsers
+	//4. Commit transaction
 	return account, nil
 }
---
+++ services/Tenant.go
@@ -0,0 +1,36 @@
+package services
+
+import (
+	"DigiPassAuthenticationApi/packages/models"
+	"errors"
+	"gorm.io/gorm"
+)
+
+type TenantService struct {
+	db *gorm.DB
+}
+
+func NewTenantService(db *gorm.DB) *TenantService {
+	return &TenantService{
+		db: db,
+	}
+}
+
+func (s *TenantService) CreateUniqueTenantSlug(db *gorm.DB) (string,error){
+	maxAttempts := 10
+
+	for i := 0; i < maxAttempts; i++ {
+		slug := models.Tenant{}.CreateSlug()
+
+		var count int64
+		err := db.Model(&models.Tenant{}).Where("slug = ?", slug).Count(&count).Error
+		if err != nil {
+			return "", err
+		}
+
+		if count == 0 {
+			return slug, nil
+		}
+	}
+	return "", errors.New("Failed to generate unique slug after multiple attempts")
+}
