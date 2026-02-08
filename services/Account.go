8d7fedb6f2c579c3d648777d403037c5d6be9dfb
Chris Jurgens <chrisjurgens12317@gmail.com>
2026-02-08 15:16:40 -0500

    Add Name back to Tenant Table

--- services/Account.go
+++ services/Account.go
@@ -50,6 +50,7 @@ func (s *AccountService) CreateAccount(name string, email string) (*models.Accou
 	tenant := &models.Tenant{
 		AccountID: account.ID,
 		Slug: slug,
+		Name: fmt.Sprintf("%s-%s", account.Name, slug)
 	}
 
 	if err := tx.Create(tenant).Error; err != nil {
